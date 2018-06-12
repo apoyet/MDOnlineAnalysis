45a4098a4e2cd3a63d28d8de6f5dd416cbc511ce
apoyet <32321729+apoyet@users.noreply.github.com>
2018-06-12 11:22:25 +0200

    Update postProcess.py
    Copy of the raw2bbb function with filling pattern filtering.

--- md2pd/postProcess.py
+++ md2pd/postProcess.py
@@ -13,3 +13,17 @@ def raw2bbbDF(rawInput):
     for i in range(3564):
         DF[i] = rawInput.dropna().apply(lambda x:x[i])
     return DF
+
+def raw2bbbDF_filter(rawInput,fill):
+    '''
+    Input: 
+    rawInput is a pandas Series containing vectors (FBCT, lumi, etc...)
+    fill is an array containing the bunches numbers to filter
+    
+    Output: 
+    A df containing the same data, splitted bbb in each column
+    '''
+    DF = pd.DataFrame()
+    for i in fill:
+        DF[i] = rawInput.dropna().apply(lambda x:x[i])
+    return DF
